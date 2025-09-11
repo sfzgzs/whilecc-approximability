f31f028e4eea459fb847123811fec115d1d16b06
sfzgzs <ghasemi.fateme33@gmail.com>
2025-09-11 17:44:06 -0400

    Add Partial proof for le_ωSup

--- WCC/Domain/OmegaCompletePartialOrder.lean
+++ WCC/Domain/OmegaCompletePartialOrder.lean
@@ -3,14 +3,12 @@ import WCC.Domain.PartialOrder
 
 open Set
 
-open Classical in
--- @[simps le ωSup]
-noncomputable instance : OmegaCompletePartialOrder (Domain X) where
-  ωSup := by
-    intro h
-    let theSup := (⋃ i, (h i).val) \ { x | x = .none ∧ ∃ i, .none ∉ (h i).val}
+
+@[simp]
+def thesup (X : Type) (h : OmegaCompletePartialOrder.Chain (Domain X)) : Domain X :={
+  val :=  (⋃ i, (h i).val) \ { x | x = .none ∧ ∃ i, .none ∉ (h i).val},
+  property := by
     have simpleProof : ((⋃ i, (h i).val) \ { x | x = .none ∧ ∃ i, .none ∉ (h i).val}) ≠ ∅ := by
-      have supp := {x | x ∈ (⋃ i, (h i).val) ∧ x ∉  { x | x = .none ∧ ∃ i, .none ∉ (h i).val}}
       by_cases hp : ∃ i, .none ∉ (h i).val
       · simp only [hp, and_true, setOf_eq_eq_singleton, ne_eq]
         obtain ⟨n, hn⟩ := hp
@@ -27,6 +25,58 @@ noncomputable instance : OmegaCompletePartialOrder (Domain X) where
         simp only [hp, and_false, setOf_false, diff_empty, iUnion_eq_empty, not_forall]
         use 0
         exact (h 0).property
-    exact ⟨theSup, simpleProof⟩
-  le_ωSup := sorry
+    exact simpleProof
+  }
+
+-- @[simp]
+theorem none_is_carried_down
+    {x y : Domain X}
+    (h₁ : x ≤ y)
+    (h₂ : .none ∈ y.val)
+  : .none ∈ x.val := by
+  simp only [LE.le, Domain.newOrder, Option.newOrder] at h₁
+  rcases h₁ with ⟨h₃, h₄⟩
+  let tmp := h₄ none h₂
+  rcases tmp with ⟨h₅, h₆, h₇⟩
+  simp only [or_self] at h₇
+  rw [h₇] at h₆
+  exact h₆
+
+theorem chain_mon
+  (h₀ : OmegaCompletePartialOrder.Chain (Domain X))
+  (n₁ n₂ : ℕ)
+  (h₁ : n₁ ≤ n₂)
+  : h₀ n₁ ≤ h₀ n₂ := by
+  exact h₀.monotone h₁
+
+noncomputable instance : OmegaCompletePartialOrder (Domain X) where
+  ωSup := thesup X
+  le_ωSup := by
+    intro h n
+    let theSup := thesup X h
+    change h n ≤ thesup X h
+    by_cases hp : ∃ i, .none ∉ (h i).val
+    · rcases hp with ⟨w, hw⟩
+      simp only [LE.le, Domain.newOrder, thesup, mem_diff, mem_iUnion, mem_setOf_eq, not_and,
+        not_exists, Option.newOrder, and_imp, forall_exists_index]
+      apply And.intro
+      · intro x h₁
+        cases x with
+        | none =>
+          use none
+          apply And.intro
+          · simp only [forall_const]
+            apply And.intro
+            · use n
+            · intro m₂
+              by_cases h₂: m₂ ≤ n
+              · simp only [not_not]
+                let ord : h m₂ ≤ h n := by
+                  apply chain_mon h m₂ n h₂
+                apply none_is_carried_down ord h₁
+              · sorry
+          · sorry
+        | some val => sorry
+      · sorry
+    · sorry
   ωSup_le := sorry
