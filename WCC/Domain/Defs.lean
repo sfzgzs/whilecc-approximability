8f5cb23682e2cfa6c23204a24c75c8f863ff978e
sfzgzs <ghasemi.fateme33@gmail.com>
2025-09-15 00:15:02 -0400

    Add lemmas for Domain.newOrder

--- WCC/Domain/Defs.lean
+++ WCC/Domain/Defs.lean
@@ -40,6 +40,59 @@ def Domain.newOrder (d₁ d₂ : Domain X) : Prop :=
   ∧
   (∀ y ∈ d₂.val, ∃ x ∈ d₁.val , Option.newOrder x y)
 
+@[simp]
+lemma Domain.newOrder.some_le {d₁ d₂ : Domain X}
+    (v : X)
+    (h : Domain.newOrder d₁ d₂)
+    : some v ∈ d₁.val → some v ∈ d₂.val := by
+  simp only [newOrder] at h
+  rcases h with ⟨h₁, h₂⟩
+  intro h₃
+  let tmp := h₁ (some v) h₃
+  simp only [Option.newOrder_some, exists_eq_right] at tmp
+  exact tmp
+
+@[simp]
+lemma Domain.newOrder.le_none {d₁ d₂ : Domain X}
+    (h : Domain.newOrder d₁ d₂)
+    : none ∈ d₂.val → none ∈ d₁.val := by
+  simp only [newOrder] at h
+  rcases h with ⟨h₁, h₂⟩
+  intro h₃
+  have tmp := h₂ none h₃
+  simp only [Option.newOrder_none_right, exists_eq_right] at tmp
+  exact tmp
+
+@[simp]
+lemma Domain.newOrder.none_not_mem {d₁ d₂ : Domain X}
+    (h : Domain.newOrder d₁ d₂)
+    : none ∉ d₁.val → none ∉ d₂.val := by
+  simp only [newOrder] at h
+  rcases h with ⟨h₁, h₂⟩
+  intro h₃
+  by_contra h₄
+  have tmp := h₂ none h₄
+  simp only [Option.newOrder_none_right, exists_eq_right] at tmp
+  contradiction
+
+
+@[simp]
+lemma Domain.newOrder.none_not_mem_le_some {d₁ d₂ : Domain X}
+    (h₁ : Domain.newOrder d₁ d₂) (h₂ : some v ∈ d₂.val)
+    : none ∉ d₁.val → some v ∈ d₁.val := by
+  simp only [newOrder] at h₁
+  intro h₅
+  have tmp := Domain.newOrder.none_not_mem h₁ h₅
+  rcases h₁ with ⟨h₃, h₄⟩
+  have ⟨w₂, hw₂₁, hw₂₂⟩  := h₄ (some v) h₂
+  cases w₂ with
+  | none =>
+    contradiction
+  | some val =>
+    simp only [Option.newOrder_some, Option.some.injEq] at hw₂₂
+    subst hw₂₂
+    exact hw₂₁
+
 
 example :
     let d₁ : Domain ℕ := ⟨{some 0}, by simp⟩
